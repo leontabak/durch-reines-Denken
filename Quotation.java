5741c0406762da55353c2f4617e0c1062d764fe6
Leon Tabak <l.tabak@ieee.org>
2018-09-27 09:56:05 -0500

    quotations and humorous stories

---
+++ Quotation.java
@@ -0,0 +1,52 @@
+package com.eonsahead.zitate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quotation {
+    private static int count = 0;
+
+    private static final int TEXT_LENGTH = 40;
+    private static final int AUTHOR_LENGTH = 40;
+
+    private String text;
+    private String author;
+
+    public Quotation( String text, String author ) {
+        this.text = text;
+        this.author = author;
+    } // Quotation( String, String )
+
+    public String getText() {
+        return this.text;
+    } // getText()
+
+    public String getAuthor() {
+        return this.author;
+    } // getAuthor()
+
+    public static List<Quotation> getQuotations(int n) {
+        List<Quotation> result = new ArrayList<>();
+
+        for( int i = 0; i < n; i++ ) {
+            char lowercase = (char) ('a' + (count % 26));
+            char uppercase = (char) ('A' + (count % 26));
+
+            count++;
+
+            StringBuilder quotationText = new StringBuilder();
+            for( int j = 0; j < TEXT_LENGTH; j++ ) {
+                quotationText.append( uppercase );
+            } // for
+
+            StringBuilder quotationAuthor = new StringBuilder();
+            for( int j = 0; j < AUTHOR_LENGTH; j++ ) {
+                quotationAuthor.append( lowercase );
+            } // for
+
+            result.add( new Quotation( quotationText.toString(), quotationAuthor.toString() ));
+        } // for
+
+        return result;
+    } // getQuotations( int )
+} // Quotation
---
+++ QuotationsDB.java
@@ -0,0 +1,393 @@
+
+  List<Quotation> quotations = new ArrayList<>();
+  
+  text = "Die Möglichkeit einer mechanischen Erklärung der ganzen Natur ist" +
+      "nicht bewiesen, ja, dass wir dieses Ziel vollkommen erreichen werden," +
+      "kaum denkbar." ;
+  
+  name = "Ludwig Boltzmann";
+  quotations.add( text, name );
+  
+  text = "Für den gläubigen Menschen steht Gott am Anfang, " +
+      "für den Wissenschaftler am Ende aller seiner Überlegungen." ;
+  
+  name = "Max Planck";
+  quotations.add( text, name );
+  
+  text = "Die Wahrheit triumphiert nie, ihre Gegner sterben nur aus." ;
+  
+  name = "Max Planck";
+  quotations.add( text, name );
+  
+  text = "Die Naturwissenschaft braucht der Mensch zum Erkennen, " +
+      "den Glauben zum Handeln." ;
+  
+  name = "Max Planck";
+  quotations.add( text, name );
+  
+  text = "Auch eine Enttäuschung, wenn sie nur gründlich und endgültig ist, " +
+      "bedeutet einen Schritt vorwärts." ;
+  
+  name = "Max Plank";
+  quotations.add( text, name );
+  
+  text = "Es hat Zeiten gegeben, in denen sich Philosophie und Naturwissenschaft fremd " +
+      "und unfreundlich gegenüberstanden. Diese Zeiten sind längst vorüber." ;
+  
+  name = "Max Planck";
+  quotations.add( text, name );
+  
+  text = "Religion und Naturwissenschaf---sie schließen sich nicht aus, " +
+      "wie manche heutzutage glauben oder fürchten, " +
+      "sondern sie ergänzen und bedingen einander." ;
+  
+  name = "Max Planck";
+  quotations.add( text, name );
+  
+  text = "Ein rein verstandesmäßiges Weltbild ganz " +
+      "ohne Mystik ist ein Unding." ;
+  
+  name = "Erwin Schrödinger";
+  quotations.add( text, name );
+  quotations.add( text, name );
+  
+  text = "Ich gehöre zu der Generation, die noch zwischen Verstand " +
+      "und Vernunft unterscheidet. Von diesem Standpunkt ist die Raumfahrt " +
+      "ein Triumph des Verstandes, aber ein tragisches Versagen der Vernunft!" ;
+  
+  name = "Max Born";
+  quotations.add( text, name );
+  
+  text = "Ideen, wie absolute Gewißheit, absolute Genauigkeit, " +
+      "endgültige Wahrheit und so fort, sind Erfindungen der Einbildungskraft " +
+      "und haben in der Wissenschaft nichts zu suchen." ;
+  
+  name = "Max Born";
+  quotations.add( text, name );
+  
+  text = "Das Wesen der Mathematik liegt in ihrer Freiheit." ;
+  
+  name = "Georg Cantor";
+  quotations.add( text, name );
+  
+  text = "In der Mathematik ist die Kunst, eine Frage zu stellen, " +
+      "höher zu bewerten als die Kunst, sie zu lösen." ;
+  
+  name = "Georg Cantor";
+  quotations.add( text, name );
+  
+  text = "In der Mathematik gibt es kein Ignorabimus." ;
+  
+  name = "David Hilbert";
+  quotations.add( text, name );
+  
+  text = "Manche Menschen haben einen Gesichtskreis vom Radius Null " +
+      "und nennen ihn ihren Standpunkt." ;
+  
+  name = "David Hilbert";
+  quotations.add( text, name );
+  
+  text = "Mathematik ist die Grundlage alles exakten naturwissenschaftlichen Erkennens." ;
+  
+  name = "David Hilbert";
+  quotations.add( text, name );
+  
+  text = "Für einen Mathematiker haben Sie zu wenig Phantasie, " +
+      "sie sollten Dichter werden." ;
+  
+  name = "David Hilbert";
+  quotations.add( text, name );
+    	
+  text = "Im großen Garten der Geometrie kann sich jeder " +
+      "nach seinem Geschmack einen Strauß pflücken." ;
+  
+  name = "David Hilbert";
+  quotations.add( text, name );
+  
+  text = "Wenn ich nach einem tausendjährigen Schlaf aufwachen würde, " +
+      "wäre meine erste Frage: `Wurde die Riemann-Hypothese bewiesen?' " ;
+  
+  name = "David Hilbert";
+  quotations.add( text, name );
+  
+  text = "Im Einzelnen möchte ich der Individualität des Lehrers eine " +
+      "weitgehende Freiheit lassen; ich glaube mehr an die Wirksamkeit " +
+      "der Persönlichkeiten als an diejenige der Methoden und " +
+      "ausgeklügelten Lehrpläne." ;
+  
+  name = "Felix Klein";
+  quotations.add( text, name );
+  
+  text = "Die Logik ist die Hygiene, deren sich der Mathematiker bedient, " +
+      "um seine Gedanken gesund und kräftig zu erhalten." ;
+  
+  name = "Hermann Weyl";
+  quotations.add( text, name );
+  
+  text = "Die Symmetrie ist diejenige Idee, mit deren Hilfe der Mensch im Laufe " +
+      "der Jahrhunderte versuchte, Ordnung, Schönheit und Vollkommenheit " +
+      "zu begreifen und zu schaffen." ;
+  
+  name = "Hermann Weyl";
+  quotations.add( text, name );
+  
+  text = "Die Ideen sind nicht verantwortlich für das, " +
+      "was die Menschen aus ihnen machen." ;
+  
+  name = "Werner Heisenberg";
+  quotations.add( text, name );
+  
+  text = "Auch das lauteste Getöse großer Ideale darf uns nicht " +
+      "verwirren und nicht hindern, den einen leisen Ton zu hören, " +
+      "auf den alles ankommt." ;
+  
+  name = "Werner Heisenberg";
+  quotations.add( text, name );
+  
+  text = "Die Naturwissenschaft beschreibt und erklärt die Natur " +
+      "nicht einfach, so wie sie `an sich' ist. Sie ist vielmehr " +
+      "ein Teil des Wechselspiels zwischen der Natur und uns selbst." ;
+  
+  name = "Werner Heisenberg";
+  quotations.add( text, name );
+  
+  text = "Das Leben muss nicht leicht sein, wenn es nur inhaltsreich ist." ;
+  
+  name = "Lise Meitner";
+  quotations.add( text, name );
+  
+  text = "Solange nur wir, die Verfolgten, die schlaflosen Nächte " +
+      "haben und nicht Ihr, solange wird es in Deutschland " +
+      "nicht besser werden." ;
+  
+  name = "Lise Meitner";
+  quotations.add( text, name );
+  
+  text = "In der Welt der Mathematik ist alles im Gleichgewicht " +
+      "und perfekt geordnet. " +
+      "Sollte man nicht dasselbe für die Welt der Realität annehmen, " +
+      "entgegen allem Anschein?" ;
+  
+  name = "Kurt Gödel";
+  quotations.add( text, name );
+
+  text = "Die Welt ist vernünftig." ;
+  
+  name = "Kurt Gödel";
+  quotations.add( text, name );
+  
+  text = "Phantasie ist wichtiger als Wissen, denn Wissen ist begrenzt." ;
+  
+  name = "Albert Einstein";
+  quotations.add( text, name );
+  
+  text = "Das Schönste, was wir erleben können, ist das Geheimnisvolle." ;
+  
+  name = "Albert Einstein";
+  quotations.add( text, name );
+  
+  text = "Die Naturwissenschaft ohne Religion ist lahm, " +
+      "die Religion ohne Naturwissenschaft ist blind." ;
+  
+  name = "Albert Einstein";
+  quotations.add( text, name );
+  
+  text = "Gott würfelt nicht." ;
+  
+  name = "Albert Einstein";
+  quotations.add( text, name );
+  
+  text = "Raffiniert ist der Herrgot, aber boshaft ist er nicht." ;
+  
+  name = "Albert Einstein";
+  quotations.add( text, name );
+  
+  text = "Thermodynamik ist ein komisches Fach. Das erste Mal, wenn man sich" +
+      "damit befasst, versteht man nichts davon. Beim zweiten Durcharbeiten" +
+      "denkt man, man hätte nun alles verstanden, mit Ausnahme von ein oder" +
+      "zwei kleinen Details. Das dritte Mal, wenn man den Stoff" +
+      "durcharbeitet, bemerkt man, dass man fast gar nichts davon versteht," +
+      "aber man hat sich inzwischen so daran gewöhnt, dass es einen nicht" +
+      "mehr stört." ;
+  
+  name = "Arnold Sommerfeld";
+  quotations.add( text, name );
+  
+  text = "Was wir heutzutage aus der Sprache der Spektren heraus hören, ist eine" +
+      "wirkliche Sphärenmusik des Atoms, ein Zusammenklingen ganzzahliger" +
+      "Verhältnisse, eine bei aller Mannigfaltigkeit zunehmende Ordnung und" +
+      "Harmonie." ;
+  
+  name = "Arnold Sommerfeld";
+  quotations.add( text, name );
+  
+  text = "Es handelt sich, so kurz wie möglich ausgedrückt darum, " +
+      "daß die Welt in Raum und Zeit in gewissem Sinne eine " +
+      "vierdimensionale nichteuklidische Mannigfaltigkeit ist." ;
+  
+  name = "Hermann Minkowski";
+  quotations.add( text, name );
+  
+  text = "Ohne Fehler kann es keine Brillanz geben." ;
+  
+  name = "Emanuel Lasker";
+  quotations.add( text, name );
+  
+  text = "Ich möchte Schüler heranbilden, die das Vermögen haben, " +
+      "selbst zu denken und Kritik zuüben." ;
+  
+  name = "Emanual Lasker";
+  quotations.add( text, name );
+  
+  text = "Wenn Sie zu wählen haben zwischen einem Genie und einem Charakter, " +
+      "vergessen Sie das Genie." ;
+  
+  name = "Carl Bosch";
+  quotations.add( text, name );
+  
+  text = "Das Denken ist auch eine Wirklichkeit, " +
+      "sogar eine der wichtigsten im Menschenleben." ;
+  
+  name = "Wilhelm Ostwald";
+  quotations.add( text, name );
+  
+  text = "Für augenblicklichen Gewinn verkaufe ich die Zukunft nicht." ;
+  
+  name = "Werner von Siemens";
+  quotations.add( text, name );
+  
+  text = "Messen ist Wissen." ;
+  
+  name = "Werner von Siemens";
+  quotations.add( text, name );
+  
+  text = "Ohne Mathematik tappt man doch immer im Dunkeln." ;
+  
+  name = "Werner von Siemens";
+  quotations.add( text, name );
+  
+  text = "Es kommt nicht darauf an, mit dem Kopf durch die Wand zu rennen, " +
+      "sondern mit den Augen die Tür zu finden." ;
+  
+  name = "Werner von Siemens";
+  quotations.add( text, name );
+  
+  text = "Von 100 Genies gehen 99 unentdeckt zugrunde." ;
+  
+  name = "Rudolf Diesel";
+  quotations.add( text, name );
+  
+  text = "Jedes Naturgesetz, das sich dem Beobachter offenbart, " +
+      "lässt auf ein höheres, noch unerkanntes schließen." ;
+  
+  name = "Alexander von Humboldt";
+  quotations.add( text, name );
+  
+  text = "Bei der Eroberung des Weltraums sind zwei Probleme zu lösen: " +
+      "die Schwerkraft und der Papierkrieg. " +
+      "Mit der Schwerkraft wären wir fertig geworden." ;
+  
+  name = "Werner von Braun";
+  quotations.add( text, name );
+  
+  text = "Wo viel Licht ist, ist starker Schatten." ;
+  
+  name = "Johann Wolfgang von Goethe";
+  quotations.add( text, name );
+  
+  text = "Auch für den Physiker ist die Möglichkeit einer Beschreibung " +
+      "in der gewöhnlichen Sprache ein Kriterium für den Grad des Verständnisses, " +
+      "das in dem betreffenden Gebiet erreicht worden ist." ;
+  
+  name = "Werner Heisenberg";
+  quotations.add( text, name );
+  
+  text = "Die Quantentheorie läßt keine völlig objektive Beschreibung der Natur mehr zu." ;
+  
+  name = "Werner Heisenberg";
+  quotations.add( text, name );
+  
+  text = "Die Quantentheorie ist so ein wunderbares Beispiel dafür, " +
+      "daß man einen Sachverhalt in völliger Klarheit verstanden " +
+      "haben kann und gleichzeitig doch weiß, daß man nur in Bildern " +
+      "und Gleichnissen von ihm reden kann." ;
+  
+  name = "Werner Heisenberg";
+  quotations.add( text, name );
+  
+  text = "Der Physiker erklärt die Geheimnisse der Natur nicht, " +
+      "er führt sie auf tieferliegende Geheimnisse zurück." ;
+  
+  name = "Carl Friedrich von Weizsäcker";
+  quotations.add( text, name );
+  
+  text = "Ein Mathematiker, der nicht irgendwie ein Dichter ist, " +
+      "wird nie ein vollkommener Mathematiker sein." ;
+  
+  name = "Karl Weierstraß";
+  quotations.add( text, name );
+  
+  text = "Wie die Musiker schon nach den ersten Akkorden Mozart, " +
+      "Beethoven, Schubert erkennen, so könnten die Mathematiker ihren Cauchy, " +
+      "Gauß, Jacobi oder Helmholtz nach wenigen Seiten erkennen." ;
+  
+  name = "Ludwig Boltzmann";
+  quotations.add( text, name );
+  
+  text = "Was beweisbar ist, soll in der Wissenschaft nicht ohne Beweis geglaubt werden." ;
+  
+  name = "Richard Dedekind";
+  quotations.add( text, name );
+  
+  text = "Es ist nicht das Wissen, sondern das Lernen, nicht das Besitzen, " +
+      "sondern das Erwerben, nicht das Dasein, sondern das Hinkommen, " +
+      "was den größten Genußgewährt." ;
+  
+  name = "Carl Friedrich Gauß";
+  quotations.add( text, name );
+  
+  text = "Nichts ist getan, wenn noch etwas zu tun übrig ist." ;
+  
+  name = "Carl Friedrich Gauß";
+  quotations.add( text, name );
+  
+  text = "Da ist das Problem, suche die Lösung. " +
+      "Du kannst sie durch reines Denken finden; " +
+      "denn in der Mathematik gibt es kein Ignorabimus." ;
+
+  name = "Carl Friedrich Gauß";
+  quotations.add( text, name );
+  
+  text = "Die ganzen Zahlen hat der liebe Gott gemacht, alles andere ist Menschenwerk." ;
+  
+  name = "Leopold Kronecker";
+  quotations.add( text, name );
+  
+  text = "Die Mathematik ist das Instrument, welches die Vermittlung bewirkt zwischen " +
+      "Theorie und Praxis, zwischen Denken und Beobachten: Sie baut die verbindende " +
+      "Brücke und gestaltet sie immer tragfähiger. " +
+      "Daher kommt es, daß unsere ganze gegenwärtige Kultur, soweit sie auf der " +
+      "geistigen Durchdringung und Dienstbarmachung der Natur beruht, " +
+      "ihre Grundlage in der Mathematik findet." ;
+  
+  name = "David Hilbert";
+  quotations.add( text, name );
+  
+  text = "Man darf nicht das, was uns unwahrscheinlich und unnatürlich erscheint, " +
+      "mit dem verwechseln, was absolut unmöglich ist." ;
+  
+  name = "Carl Friedrich Gauß";
+  quotations.add( text, name );
+  
+  text = "Die Frage ist zu gut, um sie mit einer Antwort zu verderben." ;
+  
+  name = "Robert Koch";
+  quotations.add( text, name );
+  
+  text = "Ich weiß, dass ich an der Geometrie das Glück zuerst kennengelernt habe." ;
+  
+  name = "Rudolf Steiner";
+  quotations.add( text, name );
+  
+
+
